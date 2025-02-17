5d9b115b4b943625b675438284572157ed92049b
code-888 <cedriz@hotmail.com>
2025-02-17 11:32:58 -0500

    Revert "this is a commit because there is an error when i'm doing a revert"
    This reverts commit 37a661e2644f949a4edd32a087d9589c2a8ad890.

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -10,6 +10,6 @@ shipping_cost = weight * rate
 ## Display the result
 print(f"Shipping Cost: {shipping_cost} USD")
 
-# Here is a new update by code-888
-# Here is another update by code-888
+ # Here is a new update by code-888
+
 
